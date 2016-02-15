1bb425fbdaef2f804f7d423ce48a1944ef8af4b9
Ubuntu <ubuntu@ip-172-31-28-161.us-west-2.compute.internal>
2016-02-15 06:08:02 +0000

    asdf

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-var fs = require('fs');
-//var express = require('express');
-//var logger = require('morgan');
-var app = express();
-//app.use(logger());
+var express = require('express')
+var app = express()
 
-//var htmlfile = "index.html";
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
- // var html = fs.readFileSync(htmlfile).toString('utf8');
-  response.send('html');
-});
+  response.send('Hello World!')
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-});
+})
